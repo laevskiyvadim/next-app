0a44ef355602e71ec9fca43e122a1e7135017742
vadim.laevskiy77 <vadim.laevskiy77@gmail.com>
2022-06-16 21:21:33 +0300

    init

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import { AppProps } from "../node_modules/next/app";
+import "../styles/globals.css";
+import Head from "../node_modules/next/head";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+
+  return <>
+    <Head>
+      <title>MyTop - наш лучший топ</title>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
+}
+
+export default MyApp;
