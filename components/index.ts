f1c46793610c85fbcbb931941a8ac6d1022d9d6d
vadim.laevskiy77 <vadim.laevskiy77@gmail.com>
2022-06-21 16:05:26 +0300

    no message

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export * from './Simple/Htag/Htag';
 export * from './Simple/Button/Button';
 export * from './Simple/P/P';
 export * from './Simple/Tag/Tag';
+export * from './Middle/Rating/Rating';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,18 @@
-import { Button, Htag, P, Tag } from "../components/index";
+import { useEffect, useState } from "react";
+import { Button, Htag, P, Rating, Tag } from "../components/index";
 
 export default function Home(): JSX.Element {
+  const [counter, setCounter] = useState<number>(0);
+  const [rating, setRating] = useState<number>(4);
+
   return (
     <div>
-      <Htag tag='h1'>Text</Htag>
+      <Htag tag='h1'>Кукуруза</Htag>
       <Button appearance="primary" arrow="right">Кнопка</Button>
       <Button appearance="ghost" arrow="down">Кнопка</Button>
       <P size="l"> sfasfasf </P>
       <Tag size="m" color="primary">Green</Tag>
+      <Rating rating={rating} setRating={setRating} isEditable={true}></Rating>
     </div>
   );
 }
